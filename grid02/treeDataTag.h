59c92309f698165ece7974e120ab1c64eab3d9ee
mizutani_Note <mizutani.kinoko@gmail.com>
2014-11-13 15:35:19 +0900

    Making treeDataTag.h

--- grid02/treeDataTag.h
+++ grid02/treeDataTag.h
@@ -4,6 +4,9 @@
 #include "define.h"
 
 namespace tag{
+
+typedef unsigned char byte;
+
 	typedef enum{
 		tag_end,
 		tag_byte,
@@ -20,22 +23,51 @@ namespace tag{
 	} tag_t;
 
 	class tag_basic{
+	protected:
+		tag_t type;
+		size_t size;
+		bool isArray;
+		friend return_t del(){
+			return RETURN_SUCCESS;
+		};
 	public:
 		tag_basic(){
-
+			type = tag_end;
+			isArray= false;
+			size = 0;
 		};
 		~tag_basic(){
 
-		}
+		};
 	};
 
-	class _tag_array_helper{
+	template <class T> class _tag_array_helper : public tag_basic{
+	private:
+		vector<T> data;
 	public:
 		_tag_array_helper(){
-
+			isArray = true;
 		};
 		~_tag_array_helper(){
 
+		};
+		return_t push_back(T param){
+			data.push_back(param);
+			return RETURN_SUCCESS;
+		};
+		return_t erase(size_t id){
+			data.erase(data.begin() + id);
+			return RETURN_SUCCESS;
+		};
+		bool a(size_t id){
+			return data.size() > id;
+		};
+		return_t pop_back(T &dat){
+			if(data.empty())
+				return RETURN_FAILED;
+			T dat = data[data.size() - 1];
+			data.pop_back();
+			return RETURN_SUCCESS;
 		};
 	};
 }
