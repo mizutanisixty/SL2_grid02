e5720066a28f1f01478d4e1db059d321144a1ace
mizutani_YSFH <mizutani.kinoko@gmail.com>
2014-11-13 17:26:27 +0900

    treeDataTag.h

--- grid02/treeDataTag.h
+++ grid02/treeDataTag.h
@@ -4,22 +4,21 @@
 #include "define.h"
 
 namespace tag{
+	typedef unsigned char byte;
 
-typedef unsigned char byte;
-
-	typedef enum{
-		tag_end,
-		tag_byte,
-		tag_short,
-		tag_int,
-		tag_long,
-		tag_float,
-		tag_double,
-		tag_byte_array,
-		tag_string,
-		tag_list,
-		tag_compound,
-		tag_int_array,
+	typedef enum _tag_t{
+		t_end,
+		t_byte,
+		t_short,
+		t_int,
+		t_long,
+		t_float,
+		t_double,
+		t_byte_array,
+		t_string,
+		t_list,
+		t_compound,
+		t_int_array
 	} tag_t;
 
 	class tag_basic{
@@ -32,7 +31,7 @@ typedef unsigned char byte;
 		};
 	public:
 		tag_basic(){
-			type = tag_end;
+			type = t_end;
 			isArray= false;
 			size = 0;
 		};
@@ -41,12 +40,13 @@ typedef unsigned char byte;
 		};
 	};
 
-	template <class T> class _tag_array_helper : public tag_basic{
+	template <class T, tag_t tagClassId> class _tag_array_helper : public tag_basic{
 	private:
 		vector<T> data;
 	public:
 		_tag_array_helper(){
 			isArray = true;
+			type = tagClassId;
 		};
 		~_tag_array_helper(){
 
@@ -70,5 +70,30 @@ typedef unsigned char byte;
 			return RETURN_SUCCESS;
 		};
 	};
+
+	class tag_int : tag_basic{
+	public:
+		tag_int(){};
+		~tag_int(){};
+	};
+
+	//ArrayŒnƒNƒ‰ƒX‚Ì’è‹`
+	typedef _tag_array_helper<tag_byte, t_byte_array> tag_byte_array;
+	typedef _tag_array_helper<tag_int, t_int_array> tag_int_array;
+
+	typedef union _tag_u{
+		tag_end tEnd;
+		tag_byte tByte;
+		tag_short tShort;
+		tag_int tInt;
+		tag_long tLong;
+		tag_float tFloat;
+		tag_double tDouble;
+		tag_byte_array tByteArr;
+		tag_string tString;
+		tag_list tList;
+		tag_compound tCompound;
+		tag_int_array tIntArr;
+	} tag_u;
 }
 #endif 
